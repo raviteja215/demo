85f5073f9879a97128dba2b3eb88f06503550c59
root <root@ip-172-31-15-219.ap-south-1.compute.internal>
2024-05-20 12:16:05 +0000

    demo first commit

---
+++ src/main/java/raviteja/life/App.java
@@ -0,0 +1,13 @@
+package raviteja.life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
